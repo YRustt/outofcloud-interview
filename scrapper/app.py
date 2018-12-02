926979f006bd6f17b0fd96182155bd95af83c010
Rustam Yunusov <yrustt@yandex.ru>
2018-12-02 21:16:38 +0300

    Add autoreload for scrapper development

--- scrapper/app.py
+++ scrapper/app.py
@@ -1,11 +1,15 @@
+import os
 from aiohttp import web
 
 from core.routes import setup_routes
 from scrapper.settings import GlobalConfig
 
 
+DEBUG = os.environ.get('DEBUG')
+
+
 GlobalConfig().init()
 
 application = web.Application()
 setup_routes(application)
-web.run_app(application)
+
