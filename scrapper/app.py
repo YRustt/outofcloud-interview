4b17a4119020a603129aa154a35573364b9a629a
Rustam Yunusov <yrustt@yandex.ru>
2018-12-03 23:12:05 +0300

    Add middleware for error handling

--- scrapper/app.py
+++ scrapper/app.py
@@ -1,15 +1,26 @@
-import os
 from aiohttp import web
 
 from core.routes import setup_routes
 from scrapper.settings import GlobalConfig
+from scrapper.exceptions import (
+    NotRegisteredService,
+    NotInitializedGlobalConfig,
+    NotValidRequestType,
+    NotValidTagsPath
+)
 
 
-DEBUG = os.environ.get('DEBUG')
+@web.middleware
+async def error_middleware(request, handler):
+    try:
+        response = await handler(request)
+        return response
+    except (NotRegisteredService, NotInitializedGlobalConfig, NotValidRequestType, NotValidTagsPath) as ex:
+        return web.json_response({"error": str(ex)}, status=500)
 
 
 GlobalConfig().init()
 
-application = web.Application()
+application = web.Application(middlewares=[error_middleware])
 setup_routes(application)
 
