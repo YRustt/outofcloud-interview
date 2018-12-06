be2b878bd323e145f0274085336b8edb55f7a4bc
Rustam Yunusov <yrustt@yandex.ru>
2018-12-06 15:19:22 +0300

    Add frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Switch, Route, Link } from 'react-router-dom';
 import './App.css';
+
+import News from './components/News';
+import Detail from './components/Detail';
+import Registration from './components/Registration';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <header>
+                <div className="bg-191">
+                        <div className="container">
+                                <div className="oflow-hidden color-ash font-9 text-sm-center ptb-sm-5">
+
+                                        <ul className="float-left float-sm-none list-a-plr-10 list-a-plr-sm-5 list-a-ptb-15 list-a-ptb-sm-10">
+                                                <li><a href="#">Контактная информация</a></li>
+                                        </ul>
+                                        <ul className="float-right float-sm-none list-a-plr-10 list-a-plr-sm-5 list-a-ptb-15 list-a-ptb-sm-5">
+                                                <li><a className="pl-0 pl-sm-10" href="#"><i className="ion-social-facebook"></i></a></li>
+                                                <li><a href="#"><i className="ion-social-twitter"></i></a></li>
+                                                <li><a href="#"><i className="ion-social-google"></i></a></li>
+                                                <li><a href="#"><i className="ion-social-instagram"></i></a></li>
+                                        </ul>
+
+                                </div>
+                        </div>
+                </div>
+
+                <div className="container">
+                        <a className="logo" href="index.html"><img src="images/news-logo.png" alt="Logo"/></a>
+
+                        <a className="menu-nav-icon" data-menu="#main-menu" href="#"><i className="ion-navicon"></i></a>
+
+                        <ul className="main-menu" id="main-menu">
+                                <li className="drop-down"><a href="03_single-post.html">СЕРВИСЫ<i className="ion-arrow-down-b"></i></a></li>
+                                <li><Link to="/register">ДОБАВИТЬ СЕРВИС</Link></li>
+                        </ul>
+                        <div className="clearfix"></div>
+                </div>
         </header>
+        <Switch>
+          <Route path='/news' component={News}/>
+          <Route path='/detail' component={Detail}/>
+          <Route path='/register' component={Registration}/>
+        </Switch>
       </div>
     );
   }
---
+++ frontend/src/components/OtherDetail.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import buildUrl from 'build-url';
+import './OtherDetail.css';
+
+
+class Detail extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    componentDidMount() {
+        const name = this.props.name;
+        const url = this.props.curr_news.link;
+
+        const new_url = buildUrl({
+            path: "/api/grub",
+            queryParams: { name: name, url: url }
+        });
+
+        fetch(new_url)
+            .then(response => response.json())
+            .then(data => this.setState(data));
+    }
+
+    render() {
+        if (this.state.title === undefined) {
+            return (<div>Статья загружается</div>);
+        }
+        const image = (this.state.image !== undefined ? this.state.image[0] : "https://cmkt-image-prd.global.ssl.fastly.net/0.1.0/ps/2321248/580/386/m1/fpnw/wm1/cover-.jpg?1487898325&s=6c1df3e0f086a52e0dc4e0a39073a714");
+        const title = this.props.curr_news.title;
+        const category = this.props.curr_news.category;
+        const published = this.props.curr_news.published;
+
+        return (
+            <div className="col-sm-6 h-400x">
+                <img className="h-2-3 h-sm-2-3" src={ image } alt=""/>
+                <h4 className="pt-20"><a href="#">{ title }</a></h4>
+                <ul className="list-li-mr-20 pt-10 mb-30">
+                    <ul className="list-li-mr-20">
+                        <li><i className="color-primary mr-5 font-12 ion-ios-pricetag"></i><span className="color-primary"><b>{ category }</b></span></li>
+                        <li><i className="color-primary mr-5 font-12 ion-ios-calendar"></i>{ published }</li>
+                    </ul>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Detail;
